b2b100ffdc162f1f43432d8904fad96d7fa20e45
DaianaMihaela <daianamihaela04@gmail.com>
2026-03-04 17:34:36 +0200

    Add files via upload

---
+++ data/workouts.js
@@ -0,0 +1,37 @@
+export const initialWorkouts = [
+  {
+    id: '1',
+    title: '1000 Calories',
+    duration: 60,
+    sessions: 5,
+    image: require('../assets/images/plan1.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Reduce Belly Fat in 21 Days',
+    duration: 45,
+    sessions: 4,
+    image: require('../assets/images/plan2.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Remove Belly In 7 Days',
+    duration: 30,
+    sessions: 3,
+    image: require('../assets/images/plan3.jpg'),
+  },
+  {
+    id: '4',
+    title: '8 KG Weight Loss In 1 Month',
+    duration: 50,
+    sessions: 4,
+    image: require('../assets/images/plan4.jpg'),
+  },
+  {
+    id: '5',
+    title: 'Quick Morning Workout For Flatter Belly',
+    duration: 25,
+    sessions: 3,
+    image: require('../assets/images/plan5.jpg'),
+  },
+];
